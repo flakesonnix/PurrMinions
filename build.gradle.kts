d220d2253c8df1b8bb0f55aaec2175761521c241
Lucy <lucy@example.com>
2026-09-12 14:35:44 +0200

    fix(ci): build dependencies (PurrCore, PurrItems, PurrCollections) before main build

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,9 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnly(files("../PurrCore/build/libs/purrcore-1.0.0.jar"))
-    compileOnly(files("../PurrItems/build/libs/purritems-1.0.0.jar"))
-    compileOnly(files("../PurrCollections/build/libs/PurrCollections-1.0.0.jar"))
+    compileOnly(fileTree("../PurrCore/build/libs") { include("*.jar") })
+    compileOnly(fileTree("../PurrItems/build/libs") { include("*.jar") })
+    compileOnly(fileTree("../PurrCollections/build/libs") { include("*.jar") })
 
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("com.google.code.gson:gson:2.10.1")
@@ -25,7 +25,7 @@ dependencies {
     testImplementation("io.mockk:mockk:1.13.12")
     testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    testImplementation(files("../PurrCore/build/libs/purrcore-1.0.0.jar"))
+    testImplementation(fileTree("../PurrCore/build/libs") { include("*.jar") })
 }
 
 tasks {
